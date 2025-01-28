2eaa1dac6df2e7a479a53690b93874d90794f7c6
arthur.petit-demouliere <arthur.petit-demouliere@epita.fr>
2025-01-28 10:02:46 +0100

    first commit

--- install.sh
+++ install.sh
@@ -1,29 +1,4 @@
-#!/bin/sh
-
-dot_list=(
-  bashrc \
-  config \
-  emacs \
-  gitconfig \
-  gitignore \
-  jnewsrc \
-  mozilla \
-  msmtprc \
-  muttrc \
-  signature \
-  slrnrc \
-  ssh \
-  thunderbird \
-  vim \
-  vimrc \
-  Xdefaults \
-
-  wallpaper.jpg \
-  xinitrc \
-  zshrc \
-  fehbg \
-)
-
-for f in ${dot_list[@]}; do
-  ln -s "$AFS_DIR/.confs/$f" "$HOME/.$f"
-done
+while IFS= read -r line; do
+    echo "installing $line"
+    nix profile install "nixpkgs#$line"
+done < packages.txt
