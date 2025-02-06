b6dea2a79e3d1945c9132e0e70ae3969a88862ea
arthur.petit-demouliere <arthur.petit-demouliere@epita.fr>
2025-02-06 15:59:50 +0100

    adding nvim + removing nerdfont

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ while IFS= read -r line; do
     echo "installing $line"
     nix profile install "nixpkgs#$line"
 done < packages.txt
+nix profile install --impure --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }'
