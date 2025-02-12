ab228c264c8aa1c441347b987d896943d0c080c9
arthur.petit-demouliere <arthur.petit-demouliere@epita.fr>
2025-02-12 14:05:40 +0100

    adding things

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ set -g fish_greeting
 
 if status is-interactive
     starship init fish | source
+    cd ~
 end
 
 # List Directory
@@ -17,6 +18,7 @@ abbr ... 'cd ../..'
 abbr .3 'cd ../../..'
 abbr .4 'cd ../../../..'
 abbr .5 'cd ../../../../..'
+
 
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
