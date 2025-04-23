7183a2f4ca8b1ce0935f04afd236fc4c2ab0dab4
arthur.petit-demouliere <arthur.petit-demouliere@epita.fr>
2025-04-23 10:00:23 +0200

    fixed an oupsii

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ while IFS= read -r line; do
     echo "installing $line"
     packages+="nixpkgs#$line "
 done < packages.txt
-#nix profile install --impure --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }'
+nix profile install --impure --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }'
 packages=${packages% }
 echo $packages
 nix profile install $packages
