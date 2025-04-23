262a255d533d475be99f5fa332a8e5517adf31b9
arthur.petit-demouliere <arthur.petit-demouliere@epita.fr>
2025-04-23 09:59:54 +0200

    changed update method for faster results

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+#!/run/current-system/sw/bin/bash
+packages=""
 while IFS= read -r line; do
     echo "installing $line"
-    nix profile install "nixpkgs#$line"
+    packages+="nixpkgs#$line "
 done < packages.txt
-nix profile install --impure --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }'
+#nix profile install --impure --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }'
+packages=${packages% }
+echo $packages
+nix profile install $packages
