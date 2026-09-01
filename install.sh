3e597d5f595d8bc5e530bb6770b395dd75f51bc3
knightmar <arthur.petit.d@gmail.com>
2026-09-01 18:42:11 +0200

    cleaned config

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/run/current-system/sw/bin/bash
+
+# build string from packages.txt
 packages=""
 while IFS= read -r line; do
     echo "installing $line"
     packages+="nixpkgs#$line "
 done < packages.txt
+
+# install custom font
 nix profile install --impure --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerd-fonts.jetbrains-mono'
+
 packages=${packages% }
 echo $packages
+
+# install the actuall packages string
 nix profile install $packages
+
+# run Hyprland
 Hyprland
